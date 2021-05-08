1207567b2ce6f56d074fc458ed41e7fc07df8de6
zhangjie <jiezhang@ddmarketinghub.com>
2021-05-08 15:34:32 +0800

    545

--- src/api/user.js
+++ src/api/user.js
@@ -5,7 +5,7 @@ import request from '@/utils/request'
 export function testonline() {
     console.log('66')
     return request({
-        url: '/testonline',
+        url: '/testonline222',
         method: 'post'
     })
 }
